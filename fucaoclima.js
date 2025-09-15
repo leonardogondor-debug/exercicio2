94426e4c8108e076336ce4acb60d68f712613635
leovit <leonardogondor@gmail.com>
2025-09-15 00:03:57 -0300

    corrige id do input de local e ajusta mensagens de erro no script de clima

--- fucaoclima.js
+++ fucaoclima.js
@@ -2,9 +2,18 @@ function climatempo() {
     const local = document.getElementById("local").value;
     
     if (local === "") {
-        let resultado = document.getElementById("erro digite um local"). re;
+        let resultado = document.getElementById("resultado"). textContent = "erro digite um local";
     }
 
+    else if (local === "sao paulo" || local === "Sao paulo" || local === "SAO PAULO"){
+        let resultado = document.getElementById("resultado").textContent = "o clima em sao paulo e de 20 graus";
+    }
 
+    else if (local === "rio de janeiro" || local === "Rio de janeiro" || local === "RIO DE JANEIRO"){
+        let resultado = document.getElementById("resultado").textContent = "o clima no rio de janeiro e de 30 graus";
+    }
 
+    else {
+        let resultado = document.getElementById("resultado").textContent = "local nao encontrado";
+    }
 }
