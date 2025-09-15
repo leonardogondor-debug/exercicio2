a2b3968c637b052a09d65117a08085fb0613313d
leovit <leonardogondor@gmail.com>
2025-09-14 23:43:37 -0300

    adiciona funcionalidade de busca de clima e exibe resultado

---
+++ fucaoclima.js
@@ -0,0 +1,10 @@
+function climatempo() {
+    const local = document.getElementById("local").value;
+    
+    if (local === "") {
+        let resultado = document.getElementById("erro digite um local"). re;
+    }
+
+
+
+}
